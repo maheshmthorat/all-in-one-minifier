c88280ba3014a1ad055b2f720cd66ac274982788
Mahesh M. Thorat <maheshmthorat@gmail.com>
2024-12-18 07:05:53 +0530

    Donation link update

--- admin/class-all-in-one-minifier-admin.php
+++ admin/class-all-in-one-minifier-admin.php
@@ -682,7 +682,7 @@ class All_In_One_Minifier_Admin
 								<table class="wp-list-table widefat fixed striped table-view-list">
 									<tbody id="the-list">
 										<tr>
-											<td><a href="https://rzp.io/l/maheshmthorat" target="_blank"><img width="160" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__))); ?>admin/assets/img/razorpay.svg" /></a></td>
+											<td><a href="https://buymeacoffee.com/maheshmthorat" target="_blank"><img width="160" src="<?php echo esc_url(plugin_dir_url(dirname(__FILE__))); ?>admin/assets/img/razorpay.svg" /></a></td>
 										</tr>
 										<tr>
 											<td>
@@ -973,7 +973,7 @@ class All_In_One_Minifier_Admin
 	public function aiomin_settings_link($links)
 	{
 		$url = get_admin_url() . 'admin.php?page=all-in-one-minifier-admin';
-		$settings_link = ["<a href='$url'>" . __('Settings') . '</a>', "<a href='https://rzp.io/l/maheshmthorat' target='_blank'>Say Thanks</a>"];
+		$settings_link = ["<a href='$url'>" . __('Settings') . '</a>', "<a href='https://buymeacoffee.com/maheshmthorat' target='_blank'>Say Thanks</a>"];
 		$links = array_merge(
 			$settings_link,
 			$links
